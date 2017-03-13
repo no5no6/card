213c6c6bf4a216302f2abb9ed2a2c171ab196aff
LittleChild <work_yuanyang@163.com>
2017-03-14 00:34:09 +0800

    添加tabbar图片及组件

---
+++ src/components/tabbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div >
+    <div class="titleRow tabbar" >
+      <div v-for="(item, index) in tabData" @click="tabChange(index)">
+        <div class="tabItem">
+          <div>
+            <image :style="{width: item.imageWidth ? item.imageWidth : '60px', height: item.imageHeight ? item.imageHeight : '60px'}" :src="item.checked ? item.checkedImage : item.image">
+            </image>
+            <text :style="{color: item.checked ? item.checkedTextColor : item.textColor}">{{item.text}}</text>
+          </div>
+          <div class="tabTip" :style="{background: item.tip ? 'red' : '#f5f3f3'}"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    props: {
+      tabData: {
+          type: Array,
+          default: [
+            {text: 'tab1', checked: true},
+            {text: 'tab2', checked: false},
+            {text: 'tab3', checked: false}
+          ]
+      },
+    },
+    data() {
+      return {
+
+      }
+
+    },
+    methods: {
+      tabChange(index){
+        this.tabData[index].tip = false;
+        this.tabData.forEach((item, idx) => {item.checked = index === idx;});
+      }
+    }
+  }
+</script>
+
+<style>
+  .titleRow { flex-direction: row; justify-content: space-between; align-items: center}
+  .tabbar { background: #f5f3f3; height: 150px; padding: 20px;}
+  .tabTip {width: 5px; height: 5px; border-radius: 10px; padding: 10px;}
+  .tabItem {flex-direction: row; align-items: flex-start}
+</style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -8,28 +8,37 @@
         <text class="searchText">{{seracText}}</text>
       </div>
     </div>
-    <div class="titleRow titleBar">
-        <text >111111122</text>
+    <div class="titleRow">
     </div>
+    <tabbar :tabData="this.tabData"></tabbar>
   </div>
 </template>
 
 <script>
+  import Tabbar from '../components/tabbar';
 
   export default {
     data() {
       return {
         logoUrl: './src/image/logo.png',
         serachUrl: './src/image/search.png',
-        seracText: '查词'
+        seracText: '查词',
+        tabData: [
+          {checkedImage: './src/image/home-checked.png', image: './src/image/home.png', textColor: '#8a8a8a', checkedTextColor: '#1296db', text: '首页', checked: true},
+          {checkedImage: './src/image/gift-checked.png', image: './src/image/gift.png', textColor: '#8a8a8a', checkedTextColor: '#1296db', text: '礼物', checked: false},
+          {checkedImage: './src/image/test-checked.png', image: './src/image/test.png', textColor: '#8a8a8a', checkedTextColor: '#1296db', text: '测试', checked: false},
+          {checkedImage: './src/image/set-checked.png', image: './src/image/set.png', textColor: '#8a8a8a', checkedTextColor: '#1296db', text: '设置', checked: false, tip: true}
+        ]
       }
-
     },
     methods: {
       update: function (e) {
         this.target = 'Weex'
         console.log('target:', this.target)
       }
+    },
+    components: {
+      Tabbar
     }
   }
 </script>
